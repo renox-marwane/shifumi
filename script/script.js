f3a8ed927b33362151d5bf4e673e6a626768bcd5
Nairi <marwanenairi6@gmail.com>
2026-09-19 14:32:24 +0200

    mise en place du    de l'humain et du robot + mise en place de tout les cas pour savoir qui entre le robot et l'humain gagne perd ou egaliter

--- script/script.js
+++ script/script.js
@@ -9,7 +9,56 @@ const  element = {gagner : 0,egalite : 0,perdu:0};
 const score = 0;
 boutons.forEach(function(bouton){
     bouton.addEventListener("click",function(){
-        console.log("j'ai jouer",bouton.textContent)
+        let humain = bouton.id;//bouton.id recupere grace a id [pierre feuille ciseau ]
+        const choix=["pierre","feuille","ciseaux"];//liste des choix 
+        let aleatoire =Math.random();// parcours aleatoirement un nombre entre 0 et 1 
+        let nombre = Math.floor(aleatoire * choix.length );// prend l'entier 
+        let robot = choix[nombre];
+
+        console.log("j'ai jouer :" +  humain);
+        console.log("Il a joué : " + robot)
+        if(humain =="pierre" && robot == "ciseaux" ){
+            element.gagner+=1;
+             afficherVictoire.textContent = element.gagner +  "victoire"
+            
+        }
+        else if (humain=="ciseaux" && robot == "feuille"){
+            element.gagner+=1;
+            afficherVictoire.textContent = element.gagner + "victoire"
+            
+        } else if (humain == "feuille" && robot == "pierre"){
+            element.gagner+=1;
+            afficherVictoire.textContent = element.gagner + "victoire"
+            
+        }else if (humain=="pierre" && robot == "pierre"){
+            element.egalite+=1
+            afficherEgalite.textContent = element.egalite + "égalité";
+            
+        }else if (humain=="ciseaux" && robot == "ciseaux"){
+            element.egalite+=1
+            afficherEgalite.textContent =element.egalite + "égalité";
+            
+        }else if (humain=="feuille" && robot == "feuille"){
+            element.egalite+=1
+            afficherEgalite.textContent = element.egalite + "égalité";
+            
+        }else if (humain=="pierre" && robot == "feuille"){
+            element.perdu+=1
+            afficherPerdu.textContent = element.perdu + "défaite";
+            
+        }else if (humain=="feuille" && robot == "ciseaux"){
+            element.perdu+=1
+            afficherPerdu.textContent = element.perdu + "défaite";
+            
+        }else if (humain=="ciseaux" && robot == "pierre"){
+            element.perdu+=1
+            afficherPerdu.textContent = element.perdu  + "défaite";
+            
+        }
+        console.log(element.gagner + "Victoire")
+        console.log(element.egalite + "égalité")
+        console.log(element.perdu + "défaite" )
+
+
     })
 });
-
